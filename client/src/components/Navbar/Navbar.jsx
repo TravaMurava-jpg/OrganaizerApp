2c69f49e49acf71955cda05d970aa9d102c5a1d6
TravaMurava-jpg <semenkulikov97@gmail.com>
2023-01-27 13:51:21 +0600

    changed navbar to link for not refreshing the page

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
-
+import {Link} from 'react-router-dom'
+ 
 import './Navbar.scss'
 
 const Navbar = () => {
@@ -10,20 +11,22 @@ const Navbar = () => {
     return(
         <nav>
     <div className="nav-wrapper navbar blue">
-      <a href="/" className="brand-logo">Shared TODOs</a>
+      <Link to="/" className="brand-logo">Shared TODOs</Link>
       {
         isLogin
-        ? <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="/dashboard">Personal Tasks</a></li>
-          <li><a href="/sharedtasks">Group Tasks</a></li>
-          <li><a href="/groups">My groups</a></li>
-          <li><a href="/allgroups">All groups</a></li>
-          <li><a href="/" onClick={logout}>Logout</a></li>
+        ? 
+        
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><Link to="/dashboard">Personal Tasks</Link></li>
+          <li><Link to="/sharedtasks">Group Tasks</Link></li>
+          <li><Link to="/groups">My groups</Link></li>
+          <li><Link to="/allgroups">All groups</Link></li>
+          <li><Link to="/" onClick={logout}>Logout</Link></li>
           </ul>
           
           :
           <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="/">Login</a></li>
+        <li><Link to="/">Login</Link></li>
       </ul>
       }
     </div>

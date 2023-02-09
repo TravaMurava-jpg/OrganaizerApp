2252ebc9c3e0ad9376dedc2aa85235820d43e05a
TravaMurava-jpg <semenkulikov97@gmail.com>
2023-02-09 12:00:42 +0100

    added mobile support

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -9,9 +9,13 @@ const Navbar = () => {
   const {logout, isLogin} = useContext(AuthContext)
   
     return(
+      <div>
         <nav>
     <div className="nav-wrapper navbar blue">
       <Link to="/" className="brand-logo">Shared TODOs</Link>
+      <Link to="#" className="sidenav-trigger" data-target="mobile-demo">
+			<i className="material-icons">menu</i>
+		</Link>
       {
         isLogin
         ? 
@@ -23,14 +27,29 @@ const Navbar = () => {
           <li><Link to="/allgroups">All groups</Link></li>
           <li><Link to="/" onClick={logout}>Logout</Link></li>
           </ul>
+           
           
           :
           <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to="/">Login</Link></li>
       </ul>
       }
+     
     </div>
+
   </nav>
+  {
+        isLogin
+        ? 
+  <ul className="sidenav" id="mobile-demo">
+	        <li><Link to="/dashboard">Personal Tasks</Link></li>
+          <li><Link to="/sharedtasks">Group Tasks</Link></li>
+          <li><Link to="/groups">My groups</Link></li>
+          <li><Link to="/allgroups">All groups</Link></li>
+          <li><Link to="/" onClick={logout}>Logout</Link></li>
+</ul>:  <ul className="sidenav" id="mobile-demo"> <li><Link to="/">Login</Link></li></ul>
+}
+</div>
     );
 }
 
